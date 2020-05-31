f67261b2236e8e964ae5caa3c4f90009188d0d7a
lyg <liyuangang66@gmail.com>
2020-05-31 22:59:56 +0800

    thrid6

--- ciserver.js
+++ ciserver.js
@@ -4,9 +4,9 @@ const child=require("child_process")
 var server=http.createServer()
 server.on('request',function(req,res){
 	res.end("success!!")
+	console.log("execFile")
 	child.execFile('./cibuild.sh',function(error,stdout,stderr){
 		console.log((stdout))
-		res.end("success!!2")
 	})
 })
 
