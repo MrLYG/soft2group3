f2a796fc3625971239ddc04541ab1e18d969f03b
lyg <liyuangang66@gmail.com>
2020-06-01 16:36:30 +0800

    six666

--- cibuild.sh
+++ cibuild.sh
@@ -3,8 +3,7 @@ git pull
 mvn clean package
 docker-compose down
 docker-compose up -d
-docker image rm s2g3
-docker commit soft2gropu3 s2g3
+docker image rm s2g3:latest
+docker commit soft2group3 s2g3
 sudo docker login --username=100012527846 ccr.ccs.tencentyun.com
 lygtyc521
-sudo docker tag s2g3 ccr.ccs.tencentyun.com/lyg_learn_docker/lyg_cd:
